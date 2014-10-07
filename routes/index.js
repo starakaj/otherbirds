cd867dbd38d972ae935d8dc66b6ae22cc77294dd
Sam Tarakajian <sam@cycling74.com>
2014-10-07 12:35:18 -0700

    Some cleanup, beginning implementation of an about page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('home', { title: 'Otherbirds' });
 });
 
+router.get('/about', function(req, res) {
+	res.render('about', { title: 'Aboutbirds'});
+});
+
 module.exports = router;
